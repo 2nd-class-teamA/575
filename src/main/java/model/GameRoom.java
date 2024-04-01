3b48748d8a4f07ff5d17f8fc82b0ee346c1e36dc
kuro115 <126228724+YuutaMatsuo@users.noreply.github.com>
2024-04-02 02:57:35 +0900

    途中

--- src/main/java/model/GameRoom.java
+++ src/main/java/model/GameRoom.java
@@ -1,17 +1,25 @@
 package model;
 
 import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
 
 public class GameRoom implements Serializable {
 	
 	// 0:上の句 1:下の句 2:真ん中の句
 	private User[] room = new User[3];
-	// プレイヤーの人数を管理する変数
-	private int pCount = 0;
+	// 上の句と下の句を格納するMap
+	Map<String,String> haiku = new HashMap<>();
 	
-	// プレイヤーの人数を設定する
+	int pCount = 0;
+	
+	// コンストラクタ マップを初期化する
 	public GameRoom(int pCount) {
 		this.pCount = pCount;
+		
+		haiku.put("kaminoku", null);
+		haiku.put("mannaka", null);
+		haiku.put("simonoku", null);
 	}
 	
 	// 空いている配列にCPUを入れる処理
